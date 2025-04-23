0ab7251598edc27396a3d03284f75004a792d4a1
Alsamri <azozid@yahoo.com>
2025-04-23 21:32:38 +0100

    fixed co dependies making tests fail+removed dist

--- backend/dist/src/routers/authRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import protectRoute from "../middleware/protectroute.js";
-import { login, logout, signup, userStatus, } from "../controller/authController.js";
-const router = express.Router();
-router.post("/login", login);
-router.post("/logout", logout);
-router.post("/signup", signup);
-router.get("/status", protectRoute, userStatus);
-export default router;
--- backend/dist/src/routers/messageRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import protectRoute from "../middleware/protectroute.js";
-import { sendMessage, fetchMessage, sideBarUsers, } from "../controller/messageController.js";
-const router = express.Router();
-router.get("/conversations", protectRoute, sideBarUsers);
-router.post("/send/:id", protectRoute, sendMessage);
-router.get("/:id", protectRoute, fetchMessage);
-export default router;
